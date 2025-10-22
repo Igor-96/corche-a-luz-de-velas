2de4f5cf6d12eba50769b6d971ef1e0001aa29ad
Igor-96 <118771616+Igor-96@users.noreply.github.com>
2025-10-22 17:30:38 +0200

    Update authorize.js

--- api/authorize.js
+++ api/authorize.js
@@ -1,11 +1,9 @@
 export default function handler(req, res) {
   const params = new URLSearchParams({
     client_id: process.env.GITHUB_CLIENT_ID,
-    scope: process.env.GITHUB_SCOPE || 'public_repo',
-    redirect_uri: process.env.OAUTH_REDIRECT_URI, // https://crochealuzdevelas.vercel.app/api/callback
+    scope: process.env.GITHUB_SCOPE || 'public_repo', // use 'repo' se o repositório for privado
+    redirect_uri: process.env.OAUTH_REDIRECT_URI,     // ex.: https://crochealuzdevelas.vercel.app/api/callback
   });
-  res.writeHead(302, {
-    Location: `https://github.com/login/oauth/authorize?${params.toString()}`
-  });
+  res.writeHead(302, { Location: `https://github.com/login/oauth/authorize?${params}` });
   res.end();
 }
